f412ea2cec38f240e828962dfc4bc668cfc35db2
renatomateusx <renatomateusx@gmail.com>
2022-08-10 09:42:51 -0300

    Initial Commit

---
+++ MediaLabWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaLabWeather
+//
+//  Created by Renato Mateus on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
